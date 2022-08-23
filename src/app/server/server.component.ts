e465ba8c6dc2149c42f6fa7fb4534c55a1a6f33f
jayden-liang <807570635@qq.com>
2022-08-23 23:30:49 +0800

    add component

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-server',
+    template: `<app-servers></app-servers> 
+    <p>hello world</p>`
+})
+export class ServerComponent {
+
+}
